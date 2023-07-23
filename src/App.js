c8cb37efbe04e8d4ddf07e1fa5480c6140de1cc9
Quincy Chisaina <113616008+Chisaina69@users.noreply.github.com>
2023-07-23 21:09:40 +0300

    Added db.json file and added the app.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import TransactionTable from './TransactionTable';
+import TransactionForm from './TransactionForm';
+import SearchBar from './SearchBar';
 
-function App() {
+const App = () => {
+  const [transactions, setTransactions] = useState([]);
+  const [filteredTransactions, setFilteredTransactions] = useState([]);
+
+  useEffect(() => {
+    // Fetch data from the local server running the JSON DB server
+    axios.get('http://localhost:8001/transactions')
+      .then((response) => {
+        setTransactions(response.data);
+      })
+      .catch((error) => {
+        console.error('Error fetching data:', error);
+      });
+  }, []);
+
+  const addTransaction = (newTransaction) => {
+    setTransactions([...transactions, newTransaction]);
+  };
+
+  const handleSearch = (searchTerm) => {
+    const filtered = transactions.filter(
+      (transaction) =>
+        transaction.description.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+    setFilteredTransactions(filtered);
+  };
+
+  const sortByCategory = () => {
+    const sorted = [...transactions].sort((a, b) =>
+      a.category.localeCompare(b.category)
+    );
+    setTransactions(sorted);
+  };
+
+  const sortByDescription = () => {
+    const sorted = [...transactions].sort((a, b) =>
+      a.description.localeCompare(b.description)
+    );
+    setTransactions(sorted);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Bank Transactions</h1>
+      <TransactionForm addTransaction={addTransaction} />
+      <SearchBar handleSearch={handleSearch} />
+      <TransactionTable
+        transactions={filteredTransactions.length > 0 ? filteredTransactions : transactions}
+        sortByCategory={sortByCategory}
+        sortByDescription={sortByDescription}
+      />
     </div>
   );
-}
+};
 
 export default App;
+
